d423794f4b281fda46194c6f0babc4f384c33a79
Nguyễn Hải Phong <87389297+DevPhongPhong@users.noreply.github.com>
2023-05-24 15:52:26 +0700

    fix css

--- btl_ltw/src/main/java/servlets/client/HomeServlet.java
+++ btl_ltw/src/main/java/servlets/client/HomeServlet.java
@@ -13,7 +13,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.util.List;
 
-@WebServlet("/")
+@WebServlet("")
 public class HomeServlet extends HttpServlet {
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
