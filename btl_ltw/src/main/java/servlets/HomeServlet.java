63b8229535cbf8e43322598d358a8c2f2ea92504
haidang1801 <hai0973839203@gmail.com>
2023-06-08 23:32:50 +0700

    a

--- btl_ltw/src/main/java/servlets/HomeServlet.java
+++ btl_ltw/src/main/java/servlets/HomeServlet.java
@@ -62,5 +62,5 @@ public class HomeServlet extends HttpServlet {
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		super.doPost(req, resp);
-	}
+	}a
 }
